dbdfa4010d811aa3bf8b8260b5f326d6461ce183
Nadir Nadirli <companynadirnadirli@gmail.com>
2024-01-09 03:08:58 +0400

    I made the inner side of BarberShopMapper.

--- src/main/java/com/nanemo/barbershop/model/map/BarberShopMapper.java
+++ src/main/java/com/nanemo/barbershop/model/map/BarberShopMapper.java
@@ -1,4 +1,35 @@
 package com.nanemo.barbershop.model.map;
 
-public class BarberShopMapper {
+import com.nanemo.barbershop.model.dto.BarberShopDto;
+import com.nanemo.barbershop.model.entity.BarberShop;
+import com.nanemo.barbershop.model.entity.HairDresser;
+import com.nanemo.barbershop.model.entity.Picture;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Mapper(componentModel = "spring")
+public interface BarberShopMapper {
+
+    @Mapping(target = "hairDressersId", expression = "java(this::hairDressersToHairDressersId)")
+    @Mapping(target = "picturesUrlId", expression = "java(this::picturesUrlToPicturesUrlId)")
+    BarberShopDto toBarberShopDto(BarberShop barberShop);
+    default Page<BarberShopDto> toPageBarberShopDto(Page<BarberShop> barberShop) {
+        List<BarberShopDto> dtoList = barberShop.getContent().stream()
+                .map(this::toBarberShopDto).collect(Collectors.toList());
+        return new PageImpl<>(dtoList, barberShop.getPageable(), barberShop.getTotalElements());
+    }
+
+    default Set<Long> hairDressersToHairDressersId(Set<HairDresser> hairDressers){
+        return hairDressers.stream().map(HairDresser::getHairDresserId).collect(Collectors.toSet());
+    }
+
+    default Set<Long> picturesUrlToPicturesUrlId(Set<Picture> pictures) {
+        return pictures.stream().map(Picture::getPictureId).collect(Collectors.toSet());
+    }
 }
